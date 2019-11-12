af407d66ea518a69c3600c5d7bbe6dc27cd307c0
kominaminaoki <pinkypinky0331@gmail.com>
2019-11-12 19:38:01 +0900

    comment controller

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    
+    public function store(Request $request)
+    {
+        $param=$request->validate([
+            'post_id'=>'required|exists:posts,id',
+            'body'=>'required|max:2000',
+        ]);
+
+        $post=Post::findOrFail($params['post_id']);
+        $post->comments()->create($params);
+
+        return redirect()->route('posts.show',['post'=>$post]);
+    }
 }
